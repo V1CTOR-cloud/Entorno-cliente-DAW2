b889b83db18526c7e573ea73ce0023fc258fbb48
V1CTOR-CLOUD <victorbg189@gmail.com>
2022-10-17 21:10:27 +0200

    FUNCIONA CON DBLCLICK

--- AEV 1/archivos/src/js/ejercicio1.js
+++ AEV 1/archivos/src/js/ejercicio1.js
@@ -1,6 +1,5 @@
 let container = document.getElementsByClassName('lovelace');
 
-
 for (let index = 1; index < container.length; index++) {
     const element = container[index];
     element.style.display = 'none';
@@ -10,29 +9,61 @@ for (let index = 1; index < container.length; index++) {
 for (let index = 0; index < container.length; index++) {
     if (container[index].nodeType === 1) {
         let lovelace = container[index];
-        //console.log(lovelace);
+
+        const numberSection = lovelace.firstChild.nextSibling.nextSibling.nextSibling;
+
+        // navigation Inside
 
 
+        numberSection.lastChild.previousSibling.childNodes.forEach(element => { //BOOK INSIDE
+            if (element.nodeType === 1) {
+                if (element.textContent.toUpperCase() === 'ANTERIOR') { // ANTERIOR BTN
+                    const btnBackIns = element;
+
+                    btnBackIns.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        //#region 
+                        /* 
+                            CAMBIAR POS
+                        */
+
+
+
+                    });
+                } else { // NEXT BTN
+                    const btnNextIns = element;
+                    btnNextIns.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        //#region 
+                        /* 
+                            CAMBIAR POS
+                        */
+                    });
+
+                }
+            }
+        });
 
         // Button navigation OUTSIDE
 
-        lovelace.lastChild.previousSibling.childNodes.forEach(element => {
+        lovelace.lastChild.previousSibling.childNodes.forEach(element => { //BOOK OUTSIDE
             if (element.nodeType === 1) {
                 if (element.textContent.toUpperCase() === 'ANTERIOR') {
                     const btnBackOut = element;
 
                     btnBackOut.addEventListener("click", (e) => {
                         e.preventDefault();
-                        lovelace.style.display = 'none';
+                        /* lovelace.style.display = 'none'; */
                         const category = lovelace.firstChild.nextSibling;
-                        console.log(category.textContent);
-
+                        //console.log(category.textContent);
+                        //console.log(index);
                         switch (category.textContent.toUpperCase()) {
                             case 'BIOGRAFIA':
                                 lovelace.style.display = 'none';
                                 index = 0;
                                 lovelace = container[index];
                                 lovelace.style.display = 'block';
+
                                 break;
 
                             case 'CONTRIBUCIÓN A LA INFORMÁTICA':
@@ -53,18 +84,12 @@ for (let index = 0; index < container.length; index++) {
 
                                 break;
                         }
-
-
-
                     });
                 } else {
                     const btnNextOut = element;
                     btnNextOut.addEventListener('click', (e) => {
                         e.preventDefault();
-
                         const category = lovelace.firstChild.nextSibling;
-                        console.log(category.textContent);
-
                         switch (category.textContent.toUpperCase()) {
                             case 'BIOGRAFIA':
                                 lovelace.style.display = 'none';
@@ -88,7 +113,6 @@ for (let index = 0; index < container.length; index++) {
 
                                 break;
                         }
-
                     });
                 }
             }
